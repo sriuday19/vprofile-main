54ca9c5cb2b8df2832395f20ac0534c56e0393f1
sriuday19 <sriuday1921@gamil.com>
2024-04-29 19:56:40 +0530

    added docker setup

--- Docker_setup.sh
+++ Docker_setup.sh
@@ -16,3 +16,6 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docke
 #docker-compose installation
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+#Adding used to the Docker group
+sudo usermod -a -G docker ubuntu
